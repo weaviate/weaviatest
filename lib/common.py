9cadda33e1f6501aa40345ee9d86bcb289bf5cfc
Jose Luis Franco Arza <jose@weaviate.io>
2024-09-20 15:43:06 +0200

    Add support for OPENAI_APIKEY

--- lib/common.py
+++ lib/common.py
@@ -2,6 +2,7 @@ import socket
 import string
 import random
 import weaviate
+import os
 
 
 def check_host_docker_internal(port=8080):
@@ -24,15 +25,21 @@ def get_host(port):
 
 
 def connect_to_weaviate(host, api_key, port, grpc_port):
+    openai_key = os.getenv("OPENAI_APIKEY")
+    headers = {
+        "X-OpenAI-Api-Key": openai_key,
+    }
     # Connect to Weaviate instance
     if host == "localhost":
         client = weaviate.connect_to_local(
-            host=get_host(port), port=port, grpc_port=grpc_port
+            host=get_host(port), port=port, grpc_port=grpc_port,
+            headers=headers if openai_key is not None else None,
         )
     else:
         client = weaviate.connect_to_wcs(
             cluster_url=host,
             auth_credentials=weaviate.auth.AuthApiKey(api_key=api_key),
+            headers=headers if openai_key is not None else None,
         )
     return client
 
